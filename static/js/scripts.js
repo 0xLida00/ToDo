b932347e139c1d124baf3869bb72fef0c92d8ba1
Lidao Betema <rodrigue.betema@gmail.com>
2024-09-07 18:19:29 +0100

    Added basic styling and javascript

---
+++ static/js/scripts.js
@@ -0,0 +1,5 @@
+// Example JavaScript functionality
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('JavaScript Loaded!');
+    // Add any JavaScript enhancements here
+});
